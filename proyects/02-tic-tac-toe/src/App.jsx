ac8cebacea5721e4437450619d93b14acd5c5ea2
22guerrabruno <22guerrabruno@gmail.com>
2023-09-20 15:16:59 +0200

    add 3 proyects react learning +

---
+++ proyects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,158 @@
+import { useState } from 'react'
+import './App.css'
+import Confetti from "canvas-confetti"
+
+const TURNS = {
+  X: "❌",
+  O: "🟢"
+}
+
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
+    <div onClick ={handleClick} className={className}>
+      {children}  
+    </div>
+  )
+} 
+const WINNER_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8], 
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8], 
+  [2,4,6]
+]
+
+
+function App() {
+  const [board, setBoard] = useState(()=> {
+    const savedBoard = window.localStorage.getItem('board')
+    if (savedBoard) {
+      return JSON.parse(savedBoard)
+    }
+    return Array(9).fill(null)
+
+  }
+  )
+  
+    
+    const [turn, setTurn] = useState(()=> {
+      const savedTurn = window.localStorage.getItem('turn') 
+      if (savedTurn) {
+        return savedTurn
+      } 
+      return TURNS.X  
+    })  
+
+    const [winner, setWinner] = useState(null)
+
+    const checkWinner = (boardToCheck) => {
+      for (const combo of WINNER_COMBINATIONS) {
+        const [a,b,c] = combo
+        if (boardToCheck[a] && 
+            boardToCheck[a] === boardToCheck[b] && 
+            boardToCheck[a] === boardToCheck[c]
+        ) {
+          return boardToCheck[a]
+        }
+      }
+      return null
+    }
+    const setNewGame = () => {
+      setBoard(Array(9).fill(null))
+      setTurn(TURNS.X)
+      setWinner(null)
+      window.localStorage.clear('board')
+      window.localStorage.clear('turn')
+    }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) { 
+      Confetti()
+      setWinner(newWinner)
+     
+     
+    }
+    if (newBoard.every(item => item !== null)) {
+      setWinner(false)
+    }
+  }
+
+    return (
+    <main className='board'>
+      <h1>TIC TAC TOE</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+          return (
+            <Square 
+              key={index} 
+              index={index} 
+              updateBoard={updateBoard}
+              >
+                {board[index]}
+            </Square>        
+          )
+          })
+        }
+        </section>
+        <section className="turn">
+          <Square isSelected= {turn === TURNS.X}>
+            {TURNS.X}
+          </Square>
+          <Square isSelected= {turn === TURNS.O}>
+            {TURNS.O}
+          </Square>
+      
+            <button onClick=
+            {setNewGame}>
+              Reiniciar
+            </button>
+    
+        </section>
+        {
+          winner!== null && (
+            <section className="winner">
+              <div className='text'>
+                <h2>
+                  {
+                    winner === false 
+                    ? "Empate" 
+                    : "Ganador: "
+                  }
+                </h2>
+                <header className='win'>
+                  {winner && <Square>{winner}</Square>}
+                </header>
+                <footer>
+                  <button onClick={
+                    setNewGame
+                  }>Empezar de nuevo</button>
+                </footer>
+              </div>
+            </section>
+          )
+        }
+    </main>
+  )
+}
+
+
+export default App
---
+++ proyects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  const[enabled, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  
+  useEffect(() => {
+    const handleMove = (event) => {  
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+      if (enabled) {
+        window.addEventListener('pointermove', handleMove)
+      }
+      
+      return () => {  //clean up  Method
+        window.removeEventListener('pointermove', handleMove)
+            }
+  }, [enabled])
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+
+  return (
+    <main>
+      <button onClick={()=> setEnable (!enabled)}> 
+        {enabled ? 'Desactivar' : 'Activar'} Seguir Puntero
+      </button>
+      <div style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+        />
+      
+    </main>
+  )
+}
+
+export default App
